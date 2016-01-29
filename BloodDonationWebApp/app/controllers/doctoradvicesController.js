999ed2637357bf1434cc101f06294d4bfd2e00af
Muhammad Usman <muhammad.usmaann@gmail.com>
2016-01-29 15:54:58 +0500

    HTML work

---
+++ BloodDonationWebApp/app/controllers/doctoradvicesController.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+app.controller('doctoradvicesController', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    $rootScope.layoutClass = "contact-form fixed-nav";
+    $rootScope.title = "Doctor Advice";
+    $rootScope.fixHeight = 0;
+
+}]);
---
+++ BloodDonationWebApp/app/controllers/searchresultsController.js
@@ -0,0 +1,6 @@
+﻿'use strict';
+app.controller('searchresultsController', ['$scope', '$rootScope', function ($scope, $rootScope) {
+    $rootScope.layoutClass = "contact-form fixed-nav";
+    $rootScope.title = "Search Result";
+    $rootScope.fixHeight = 0;
+}]);
